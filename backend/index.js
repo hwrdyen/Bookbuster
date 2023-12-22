4649a747f3163a6be0fad341b9d575fbd70e1bdc
hwrdyen <hwrdpvt@gmail.com>
2023-12-22 00:06:18 -0500

    Added CORS middleware

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,24 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
-// this will allow express to use JSON body now
+// Middleware for parsing request body: this will allow express to use JSON body now
 app.use(express.json());
+
+// Middleware for handling CORS policy
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
 
 app.get("/", (request, response) => {
   console.log(request);
