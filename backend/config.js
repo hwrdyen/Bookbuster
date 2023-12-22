0e5ef16928ef0ffb31a190c0254d559948e4a301
hwrdyen <hwrdpvt@gmail.com>
2023-12-21 23:17:28 -0500

    Initialized backend, Connected with MongoDB, Created Book Schema, Model, first POST request

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 8080;
+
+export const mongoDBURL =
+  "mongodb+srv://root:root@bookbuster.zzijngf.mongodb.net/?retryWrites=true&w=majority";
---
+++ backend/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(200).send(`Welcome to Bookbuster!`);
+});
+
+app.post("/book", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: `Send all required fields: title, author. publishYear`,
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+  } catch (error) {
+    console.log(error.message);
+    return response.status(500).send({ message: error.message });
+  }
+  JSON.stringify(request.body);
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log(`App connected to database`);
+
+    // only want express server to run if data connection is successful
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
